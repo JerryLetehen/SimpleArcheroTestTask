b7fcf0f9a497eb8f24f6cc5243cb518322d1f333
RuslanDudnikov <jerryletehen@gmail.com>
2020-08-16 22:54:14 +0300

    add Player, PlayerMover

---
+++ Assets/Scripts/Game/Player/PlayerMover.cs
@@ -0,0 +1,23 @@
+﻿using Configs;
+using UnityEngine;
+
+namespace Game.Player
+{
+    public class PlayerMover : MonoBehaviour
+    {
+        [SerializeField] private PlayerMoverSettings settings;
+        [SerializeField] private Rigidbody rb;
+
+        private void Update()
+        {
+            if (Input.anyKey == false)
+            {
+                return;
+            }
+            
+            var verticalAxis = Input.GetAxis("Vertical");
+            var horizontalAxis = Input.GetAxis("Horizontal");
+            rb.velocity = new Vector3(horizontalAxis * settings.Speed, 0f, verticalAxis * settings.Speed);
+        }
+    }
+}
